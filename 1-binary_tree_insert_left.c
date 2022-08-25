a38934d1e17d7032f36bb9272c6378b80598bd7d
Biruk Ephrem <birukeph@gmail.com>
2022-08-26 00:00:40 +0300

    fixed other bugs

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->left = parent->left;
 	new->right = NULL;
 	new->parent = parent;
-	
+
 	parent->left = new;
 
 	return (new);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new->left = NULL;
 	new->right = parent->right;
 	new->parent = parent;
-	
+
 	parent->right = new;
 
 	return (new);
