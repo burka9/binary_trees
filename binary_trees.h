52f212b7a59db8505e794f1f9fbe5664ac8ee676
Biruk Ephrem <birukeph@gmail.com>
2022-08-26 00:09:00 +0300

    task 12

--- binary_trees.h
+++ binary_trees.h
@@ -51,5 +51,6 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 #endif
